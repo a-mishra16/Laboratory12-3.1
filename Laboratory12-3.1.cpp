25458c1ce218d838dede38223ee6263e61886ed3
Aanika Mishra <a-mishra@onu.edu>
2024-12-05 09:39:39 -0500

    Initialized Visual Studios Project

---
+++ Laboratory12-3.1.cpp
@@ -0,0 +1,7 @@
+/*
+* Program: Laboratory12-3.1.cpp
+* Author: Aanika Mishra
+* Date 12-5-2024
+* Description: This program will read in text files and display the word count and character count
+*/
+
